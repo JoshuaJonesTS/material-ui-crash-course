17627363c852ab7a0cbdd193bc5bdc7f1410f6a3
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-30 15:00:09 -0800

    Import util variable into component

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,12 +1,13 @@
 import Paper from "@mui/material/Paper";
 import { Grid } from "@mui/material";
+import { imageAddress } from "./TourCard.utils";
 
 const TourCard = () => {
     return (
         <Grid item xs={3}>
             <Paper elevation={3}>
                 <img 
-                    src=''
+                    src={imageAddress}
                     alt=''
                 >
                 </img>
