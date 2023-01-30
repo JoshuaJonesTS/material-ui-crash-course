37f822907fd2362c2031bc3fd2a513f8e275ae8d
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-30 15:17:53 -0800

    Add title and duration to TourCard

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,6 +1,8 @@
 import Paper from "@mui/material/Paper";
 import { Grid } from "@mui/material";
 import { imageAddress } from "./TourCard.utils";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const TourCard = () => {
     return (
@@ -11,6 +13,21 @@ const TourCard = () => {
                     alt=''
                 >
                 </img>
+                <Box paddingX={1}>
+                    <Typography variant='subtitle1' component='h2'>
+                        Immerse into the Falls
+                    </Typography>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}
+                    >
+                    <Typography variant='body2' component='p'>
+                        5 hours
+                    </Typography>
+                    </Box>
+                </Box>
             </Paper>
         </Grid>
     );
