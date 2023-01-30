b644cc1874fccb95638f85b0709b1eccadbd422b
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-30 14:18:23 -0800

    Use MUI Container and Grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
 import TourCard from './components/TourCard';
+import Container from '@mui/system/Container';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <div className="App">
-      <TourCard></TourCard>
+      <Container>
+        <Grid container spacing={5}>
+          <TourCard></TourCard>
+        </Grid>
+      </Container>
     </div>
   );
 }
