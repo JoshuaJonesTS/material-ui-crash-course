06959996865509651e61510637d0968be1e05047
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-31 17:39:07 -0800

    Import and use ImageCollage componenet

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,6 +1,7 @@
 import { Typography } from "@mui/material";
 import Container from "@mui/system/Container";
 import Box from "@mui/material/Box";
+import ImageCollage from "../components/ImageCollage";
 
 const Tour = () => {
     return (
@@ -8,9 +9,10 @@ const Tour = () => {
             <Typography variant='h3' component='h1' marginTop={3}>
                 Explore the World in Vegas
             </Typography>
-            <Box marginTop={3} sx={{displayFlex: 'flex'}}>
+            <Box marginTop={3} sx={{display: 'flex'}}>
                 <img src='https://media.timeout.com/images/105124791/750/422/image.jpg' alt='' style={{height: '325px'}}/>
             </Box>
+            <ImageCollage />
         </Container>
     );
 }
