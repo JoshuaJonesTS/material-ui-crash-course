acfe0beb952024d03f2144a2b42daf0b8d3a5f1a
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-31 16:00:24 -0800

    Add heading to Tour.js page

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,8 +1,13 @@
+import { Typography } from "@mui/material";
+import Container from "@mui/system/Container";
+
 const Tour = () => {
     return (
-        <div>
-            Hello
-        </div>
+        <Container sx={{ width: 900 }}>
+            <Typography variant='h3' component='h1' marginTop={3}>
+                Explore the World in Vegas
+            </Typography>
+        </Container>
     );
 }
 
