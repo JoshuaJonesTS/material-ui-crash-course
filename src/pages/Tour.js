deac286591c9c5cef4490d443afb208b173f6632
JoshuaJonesTS <joshua@tailoredsoftware.us>
2023-01-31 16:55:41 -0800

    Add an image

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material";
 import Container from "@mui/system/Container";
+import Box from "@mui/material/Box";
 
 const Tour = () => {
     return (
@@ -7,6 +8,9 @@ const Tour = () => {
             <Typography variant='h3' component='h1' marginTop={3}>
                 Explore the World in Vegas
             </Typography>
+            <Box marginTop={3} sx={{displayFlex: 'flex'}}>
+                <img src='https://www.travelandleisure.com/thmb/655_7ZWBV7JsuIkj270XXpHYXxA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/las-vegas-nevada-LVTG0221-9fbc1de25545444ead7dd48d2a6b1056.jpg' alt='' style={{height: '325px', width: '500px'}}/>
+            </Box>
         </Container>
     );
 }
